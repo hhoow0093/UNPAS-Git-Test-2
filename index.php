94272f9580451a1e75e18a03ec2c829a9aa259f6
hhoow0093 <howarddai2005@gmail.com>
2024-06-29 14:23:24 +0700

    menambahkan list dalam index.php

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique quaerat architecto dicta eos? Ex illum odit earum quasi voluptate eaque architecto expedita distinctio facilis perspiciatis, inventore soluta illo? Nobis, cupiditate.</p>
+    <ul>
+        <li>1. satu</li>
+        <li>2. dua</li>
+    </ul>
 </body>
 </html>
